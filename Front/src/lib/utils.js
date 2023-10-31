d8535f2a72cbf62092068e189fb7960420ae08bc
pythoncraft2 <pythoncraft2@gmail.com>
2023-10-31 12:26:35 +0100

    chart dates

--- Front/src/lib/utils.js
+++ Front/src/lib/utils.js
@@ -16,7 +16,7 @@ export async function POST(route, donnes, url = 'http://127.0.0.1:4002') {
 }
 
 export async function GET(url, base = "http://127.0.0.1:4002") {
-    const res = await fetch(base + "/api/users/d65f849a-a3c0-4d93-94de-58c0cdbe4016")
+    const res = await fetch(base + url)
         .then(e => e.json())
         .then((e) => e)
     return res;
--- Front/src/routes/+page.svelte
+++ Front/src/routes/+page.svelte
@@ -1,249 +1,169 @@
 <script>
   import "./register/auth.scss";
-  import FusionCharts from "fusioncharts";
-  import Charts from "fusioncharts/fusioncharts.charts";
-
-  import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
-  import SvelteFC, { fcRoot } from "svelte-fusioncharts";
-
-  import dataSource from "./data.js";
-  import moment from "moment";
-  import SvelteHeatmap from "svelte-heatmap";
-  import Timeseries from 'fusioncharts/fusioncharts.timeseries';
   import NavBar from "$lib/navbar/NavBar.svelte";
-	import data from "./data.json";
-  import LineChart from "./LineChart.svelte";
+  import FusionCharts from "fusioncharts";
+  import Timeseries from "fusioncharts/fusioncharts.timeseries";
+  import SvelteFC, { fcRoot } from "svelte-fusioncharts";
+  import { GET } from "../lib/utils"
+
+  fcRoot(FusionCharts, Timeseries);
+
+  let specificUserID;
+  let employees = [];
+  let valeurRecherge;
+  let value;
+
+  const requestDataUserID = (id) => {
+    console.log("ID", id);
+    const rep = GET(`/api/workingtimes`)
+    .then(v => {
+      v.data.map((w) => {
+        var date1 = new Date(w.start);
+        console.log(date1);
+        var date2 = new Date(w.end);
+
+        // Convertissez les dates en timestamps Unix (en millisecondes)
+        var timestamp1 = date1.getTime();
+        var timestamp2 = date2.getTime();
+
+        // Calculez la différence en millisecondes
+        var differenceInMilliseconds = timestamp2 - timestamp1;
+
+        // Convertissez la différence en heures
+        var differenceInHours = differenceInMilliseconds / (1000 * 60 * 60);
+
+        console.log("Différence en heures : " + differenceInHours);
+
+        console.log("ID:", id);
+        
 
 
+        // console.log(w.end, w.start);
+      })
+    })
+  }
+
+  $: valeurRecherge = employees.filter((a) => a.name.toLowerCase().includes(value || ''));
 
   let promise,
-    jsonify = res => res.json(),
-    dataFetch = fetch('https://s3.eu-central-1.amazonaws.com/fusion.store/ft/data/area-chart-with-time-axis-data.json').then(jsonify),
-    schemaFetch = fetch('https://s3.eu-central-1.amazonaws.com/fusion.store/ft/schema/area-chart-with-time-axis-schema.json').then(jsonify);
+    jsonify = (res) => res.json(),
+    dataFetch = fetch(
+      "/data.json"
+    ).then(jsonify),
+    schemaFetch = fetch(
+      "https://s3.eu-central-1.amazonaws.com/fusion.store/ft/schema/area-chart-with-time-axis-schema.json"
+    ).then(jsonify);
+    let getListEmployee = fetch(
+      "http://localhost:4002/api/users"
+    ).then(jsonify)
+    .then((v) => {
+      employees = v.data
+    });
 
+    dataFetch.then(e => console.log(e));
   promise = Promise.all([dataFetch, schemaFetch]);
 
   const getChartConfig = ([data, schema]) => {
-    const fusionDataStore = new FusionCharts.DataStore();
-    const fusionTable = fusionDataStore.createDataTable(data, schema);
-
-
-   const dataSource = {
-  "chart": {
-    "showlegend": 0
-  },
-  "caption": {
-    "text": "Daily Visitors Count of a Website"
-  },
-  "yaxis": [
-    {
-      "plot": {
-        "value": "Daily Visitors",
-        "type": "area"
-      },
-      "title": "Daily Visitors Count",
-      "format": {
-        "suffix": "k"
-      }
-    }
-  ]
-};
-   dataSource.data = fusionTable;
+    const fusionDataStore = new FusionCharts.DataStore(),
+      fusionTable = fusionDataStore.createDataTable(data, schema);
 
     return {
-      type: 'timeseries',
-      width: '100%',
+      type: "timeseries",
+      width: "70%",
       height: 450,
-      renderAt: 'chart-container',
-      dataSource,
-    };
-  };
-
-  // Generate a random number
-  function rand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  // Generate dummy data for a heatmap
-  function generateFakeData() {
-    const data = [];
-    const date = moment().subtract(1, "year");
-    const now = moment();
-
-    while (date.isBefore(now)) {
-      data.push({
-        date: date.toDate(),
-        value: rand(0, 4),
-      });
-
-      date.add(1, "day");
-    }
-
-    return data;
-  }
-
-
-  const fakeData = generateFakeData();
-
-  fcRoot(FusionCharts, Charts, FusionTheme, Timeseries);
-
-  let chartObj,
-    chartConfig = {
-      id: "column-chart",
-      type: "column2d",
-      width: "600",
-      height: "400",
       renderAt: "chart-container",
-      dataSource
+      dataSource: {
+        language: "fr",
+        data: fusionTable,
+        chart: {
+          showLegend: 0,
+          theme: "candy"
+        },
+        caption: {
+          text: "Employee Daily Hours",
+        },
+        yAxis: [
+          {
+            plot: {
+              value: "Daily Visitors",
+              type: "column",
+            },
+            title: "Daily Visitors (in thousand)",
+          },
+        ],
+      },
     };
-
-  const changeChartTypeHandler = (arg) => {
-    chartObj.chartType(arg);
   };
-
-  console.log(fakeData);
 </script>
-    <div class="bg">
-</div>
-<NavBar />
-<LineChart {data} />
-<div class="time-manager">
-  <div class="time-manager-container">
-    <div id="chart-container">
-      <SvelteFC {...chartConfig} bind:chart={chartObj} />
-    </div>
-    <div>
-      <div class="change-type">
-        <div id="radio1">
-          <input
-            name="theme-selecter"
-            id="radioButton1"
-            type="radio"
-            on:change={() => {
-              changeChartTypeHandler("column2d");
-            }}
-            checked="checked"
-          />
-          <label for="radioButton1">Column 2D Chart</label>
-        </div>
-        <div id="radio2">
-          <input
-            name="theme-selecter"
-            id="radioButton2"
-            type="radio"
-            on:change={() => {
-              changeChartTypeHandler("bar2d");
-            }}
-          />
-          <label for="radioButton2">Bar 2D Chart</label>
-        </div>
-        <div id="radio3">
-          <input
-            name="theme-selecter"
-            id="radioButton3"
-            type="radio"
-            on:change={() => {
-              changeChartTypeHandler("pie2d");
-            }}
-          />
-          <label for="radioButton4">Pie 2D Chart</label>
-        </div>
-        <div id="radio3">
-          <input
-            name="theme-selecter"
-            id="radioButton4"
-            type="radio"
-            on:change={() => {
-              changeChartTypeHandler("timeseries");
-            }}
-          />
-          <label for="radioButton4">Timeserie Chart</label>
-        </div>
-      </div>
-    </div>
 
-    <div class="container">
-      <SvelteHeatmap
-        allowOverflow={true}
-        cellGap={5}
-        cellRadius={1}
-        colors={["#a1dab4", "#42b6c4", "#2c7fb9", "#263494"]}
-        data={fakeData}
-        dayLabelWidth={20}
-        emptyColor={"#ecedf0"}
-        endDate={moment().toDate()}
-        monthGap={20}
-        monthLabelHeight={20}
-        startDate={moment().subtract(5, "months").toDate()}
-        view={"monthly"}
-      />
+<div class="bg" />
+<NavBar />
+<div class="container">
+  <div class="time-manager">
+    <div id="chart-container">
+      {#await promise}
+        <p>Fetching data and schema...</p>
+      {:then value}
+        <SvelteFC {...getChartConfig(value)} />
+      {:catch error}
+        <p>Something went wrong: {error.message}</p>
+      {/await}
     </div>
   </div>
-
+</div>
   <div class="bloc-recherche">
     <div class="recherche">
-        <div class="form-floating">
-            <input
-                type="email"
-                class="form-control"
-                id="floatingInput"
-                placeholder="name@example.com"
-                required
-            />
-            <label for="floatingInput">Rechercher un employé</label>
-        </div>
-      <div class="liste-employe">
-        <button>hello</button>
-        <button>hello</button>
-        <button>hello</button>
-        <button>hello</button>
-        <button>hello</button>
-        <button>hello</button>
-        <button>hello</button>
+      <div class="form-floating">
+        <input
+          type="email"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          bind:value
+          required
+        />
+        <label for="floatingInput">Rechercher un employé</label>
+      </div>
+       <div class="liste-employe">
+        {#if valeurRecherge}
+          {#each valeurRecherge as employe}
+            <button on:click={() => requestDataUserID(employe.id)}>{employe.name}</button>
+          {/each}
+        {/if}
       </div>
     </div>
   </div>
-</div>
 
-<!-- <div id="chart-container" >
-  {#await promise}
-    <p>Fetching data and schema...</p>
-  {:then value}
-    <SvelteFC
-    {...chartConfig}
-    />
-  {:catch error}
-    <p>Something went wrong: {error.message}</p>
-  {/await}
-</div> -->
 
 <style>
-:global(body) {
+  :global(body) {
     display: flex;
     justify-content: center;
-}
+  }
 
-button {
-  background-image: linear-gradient(#2e2e2f, #1a1f27);
-  border: 0;
-  border-radius: 4px;
-  box-shadow: rgba(0, 0, 0, .3) 0 5px 15px;
-  box-sizing: border-box;
-  color: #fff;
-  cursor: pointer;
-  font-family: Montserrat,sans-serif;
-  font-size: .9em;
-  margin: 5px;
-  padding: 10px 15px;
-  text-align: center;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-}
-  
+  button {
+    background-image: linear-gradient(#2e2e2f, #1a1f27);
+    border: 0;
+    border-radius: 4px;
+    box-shadow: rgba(0, 0, 0, 0.3) 0 5px 15px;
+    box-sizing: border-box;
+    color: #fff;
+    cursor: pointer;
+    font-family: Montserrat, sans-serif;
+    font-size: 0.9em;
+    margin: 5px;
+    padding: 10px 15px;
+    text-align: center;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+  }
+
   .container {
-    border: 1px solid;
     padding: 20px;
+    overflow: auto;
+    display: flex;
+    justify-content: center;
   }
 
   .time-manager {
@@ -268,6 +188,4 @@ button {
     justify-content: center;
     flex: 1;
   }
-
 </style>
-
